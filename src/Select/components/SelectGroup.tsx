1f7d161746189f9868be7ee680e94efc2ac66f5b
dexkode4 <dexkode4@gmail.com>
2022-05-16 20:53:20 +0100

    update

--- src/Select/components/SelectGroup.tsx
+++ src/Select/components/SelectGroup.tsx
@@ -1,15 +1,16 @@
-import { Box, Heading } from '@chakra-ui/react';
+import { Box, Heading, HeadingProps } from '@chakra-ui/react';
 import React from 'react';
 
 interface SelectGroupProps {
 	children: React.ReactNode;
 	label: string;
+    labelStyle?: HeadingProps;
 }
 
-export const SelectGroup = ({ children, label }: SelectGroupProps) => {
+export const SelectGroup = ({ children, label, labelStyle }: SelectGroupProps) => {
 	return (
 		<Box>
-			<Heading size='sm'>{label}</Heading>
+			<Heading size='sm' {...labelStyle}>{label}</Heading>
 			<Box>{children}</Box>
 		</Box>
 	);
