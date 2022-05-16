b9abebb86f74d1f6c95d8c6adcd62a14f2c46e55
dexkode4 <dexkode4@gmail.com>
2022-05-16 09:27:18 +0100

    update

--- src/Select/Select.tsx
+++ src/Select/Select.tsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { SelectProvider } from './context';
 import { SelectWrapper } from './components/SelectWrapper';
+import { BoxProps, forwardRef } from '@chakra-ui/react';
 
 export type variant = 'native' | 'outline' | 'filled' | 'flushed' | 'unstyled';
-export interface SelectProps {
+export interface SelectProps extends BoxProps {
 	children: React.ReactNode;
 	variant?: variant;
 	icon?: React.ReactElement<any>;
 	isLoading?: boolean;
 }
 
-export const Select = ({ children }: SelectProps) => {
+export const Select = forwardRef<SelectProps, 'div'>((props, _ref) => {
+	const { children, ...rest } = props;
 	return (
 		<SelectProvider>
-			<SelectWrapper>{children}</SelectWrapper>
+			<SelectWrapper {...rest}>{children}</SelectWrapper>
 		</SelectProvider>
 	);
-};
+});
--- src/Select/components/SelectWrapper.tsx
+++ src/Select/components/SelectWrapper.tsx
@@ -1,19 +1,26 @@
 import React, { ReactNode, useContext, useRef } from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, BoxProps, forwardRef } from '@chakra-ui/react';
 import { SelectContext } from '../context';
 import { useClickOutside } from '../hooks/use-click-outside';
-interface SelectWrapperProps {
+interface SelectWrapperProps extends BoxProps {
 	children: ReactNode;
 }
 
-export const SelectWrapper = ({ children }: SelectWrapperProps) => {
-	const wrapperRef = useRef(null);
-    const { toggleDropdown } = useContext(SelectContext);
+export const SelectWrapper = forwardRef<SelectWrapperProps, 'div'>(
+	(props, _ref) => {
+		const { children, ...rest } = props;
+		const wrapperRef = useRef(null);
+		const { toggleDropdown } = useContext(SelectContext);
 
-	const doSomething = () => {
-        toggleDropdown(false)
-	};
-	useClickOutside(wrapperRef, doSomething);
+		const doSomething = () => {
+			toggleDropdown(false);
+		};
+		useClickOutside(wrapperRef, doSomething);
 
-	return <Box w="max-content" ref={wrapperRef}>{children}</Box>;
-};
+		return (
+			<Box w='max-content' {...rest} ref={wrapperRef}>
+				{children}
+			</Box>
+		);
+	},
+);
